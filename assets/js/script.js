58e991bb062a1f1d549b3f8a6a6df241b96de999
NobelAssefa <87548061+NobelAssefa@users.noreply.github.com>
2022-12-01 10:10:52 +0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+
+
+var swiper = new Swiper(".slide-logo", {
+  loop: true,
+  autoplay: {
+    delay: 20,
+    disableOnInteraction: false
+  },
+  slidesPerView: 6,
+  speed: 2000,
+  spaceBetween: 15,
+  grabCursor: true,
+  mousewheelControl: true,
+  keyboardControl: true,
+
+});
+var swiper = new Swiper(".slide-log2", {
+  loop: true,
+  autoplay: {
+    delay: 20,
+    disableOnInteraction: false
+  },
+  slidesPerView: 6,
+  speed: 2500,
+  spaceBetween: 15,
+  grabCursor: true,
+  mousewheelControl: true,
+  keyboardControl: true,
+
+});
+
+
+
